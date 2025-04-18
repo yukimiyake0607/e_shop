2642e695a13e07b1e6dc4659814a49fc5c72c9ea
yukimiyake0607 <yukimiyake38@gmail.com>
2025-04-18 16:40:36 +0900

    update: admin_product_upload_controllerにFirestoreRepositoryの操作を追加

--- lib/src/features/products_admin/presentation/admin_product_upload_controller.dart
+++ lib/src/features/products_admin/presentation/admin_product_upload_controller.dart
@@ -1,23 +1,46 @@
+import 'package:e_shop/src/features/products/data/products_repository.dart';
 import 'package:e_shop/src/features/products/domain/product.dart';
 import 'package:e_shop/src/features/products_admin/data/image_upload_repository.dart';
+import 'package:e_shop/src/routing/app_router.dart';
+import 'package:e_shop/src/utils/notifier_mounted.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'admin_product_upload_controller.g.dart';
 
 @riverpod
-class AdminProductUploadController extends _$AdminProductUploadController {
+class AdminProductUploadController extends _$AdminProductUploadController
+    with NotifierMounted {
   @override
-  FutureOr<void> build() {}
+  FutureOr<void> build() {
+    ref.onDispose(setUnmounted);
+  }
 
   Future<void> upload(Product product) async {
     try {
       state = const AsyncLoading();
+
+      // Firebase Storageに画像をアップロード
       final downloadUrl = await ref
           .read(imageUploadRepositoryProvider)
           .uploadProductImageFromAsset(product.imageUrl, product.id);
+
+      // Firestoreに商品情報を保存
+      await ref
+          .read(productsRepositoryProvider)
+          .createProduct(product.id, downloadUrl);
+
       state = const AsyncData(null);
+
+      ref
+          .read(goRouterProvider)
+          .goNamed(
+            AppRoute.adminEditProduct.name,
+            pathParameters: {'id': product.id},
+          );
     } on Exception catch (e, st) {
-      state = AsyncError(e, st);
+      if (mounted) {
+        state = AsyncError(e, st);
+      }
     }
   }
 }
